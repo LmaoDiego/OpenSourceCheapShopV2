16510b7567c3b2b4045eccc8ae77d0506813fd63
LmaoDiego <71909022+LmaoDiego@users.noreply.github.com>
2021-05-06 15:33:28 -0500

    Add files via upload

---
+++ CheapShop/src/main/java/com/opso/cheapshop/CheapshopApplication.java
@@ -0,0 +1,13 @@
+package com.opso.cheapshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheapshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheapshopApplication.class, args);
+    }
+
+}
